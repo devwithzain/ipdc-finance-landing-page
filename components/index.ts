0947587014b4322d1893280545192186ba7982f0
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-09 19:14:06 +0500

    Exported different components

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Home } from './Home';
+export { default as Button } from './Button';
+export { default as Hero } from './Hero';

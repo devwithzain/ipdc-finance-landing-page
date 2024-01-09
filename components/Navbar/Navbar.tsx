19cb53238cefd0216541ce97335fa687ec5a2b3b
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-09 19:14:26 +0500

    Navbar component completed

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,14 +1,33 @@
-import { Button } from "@components";
-import { navLinks } from "@constants";
-import { logo, magnifier } from "@public";
-import Image from "next/image";
+"use client";
 import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import { Button } from "@components";
+import { navVariants } from "@motion";
+import { navLinks } from "@constants";
+import { LinkHover } from "@animation";
+import { logo, magnifier } from "@public";
+import { useMotionValueEvent, useScroll, motion } from "framer-motion";
 
 export default function Navbar() {
+	const [hidden, setHidden] = useState(false);
+	const { scrollY } = useScroll();
+
+	useMotionValueEvent(scrollY, "change", (latest) => {
+		const previous = scrollY.getPrevious();
+		if (latest > previous) {
+			setHidden(true);
+		} else {
+			setHidden(false);
+		}
+	});
 	return (
-		<div>
-			<div className="w-full flex items-center justify-between py-[20rem]">
-				<div className="">
+		<>
+			<motion.nav
+				className="w-full flex items-center justify-between h-[10vh] px-[100rem] sticky top-0 z-10 bg-white"
+				variants={navVariants}
+				animate={hidden ? "hidden" : "vissible"}>
+				<div>
 					<Image
 						src={logo}
 						alt="logo"
@@ -18,13 +37,11 @@ export default function Navbar() {
 				</div>
 				<div className="flex gap-[10rem] font-medium tracking-[1rem] uppercase">
 					{navLinks.map((item) => (
-						<div
-							className=""
-							key={item.id}>
+						<div key={item.id}>
 							<Link
 								href={`#${item.link}`}
-								className="text-[17rem]">
-								{item.title}
+								className="text-[18rem] font-bold text-[#676767]">
+								<LinkHover>{item.title}</LinkHover>
 							</Link>
 						</div>
 					))}
@@ -38,9 +55,12 @@ export default function Navbar() {
 							className="w-[25px] object-cover"
 						/>
 					</div>
-					<Button title="iSERVICES" />
+					<Button
+						title="iSERVICES"
+						className="bg-[#ED017F]"
+					/>
 				</div>
-			</div>
-		</div>
+			</motion.nav>
+		</>
 	);
 }

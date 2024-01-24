b50359c2f24cb55811344af45eaf85abd18aa0c8
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-01-24 09:14:06 +0500

    Exported different components

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,13 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Home } from './Home';
+export { default as Footer } from './Footer';
+export { default as NewsLetter } from './NewsLetter';
+export { default as Result } from './Result';
+export { default as Experience } from './Experience';
+export { default as Partner } from './Partner';
+export { default as Glance } from './Glance';
+export { default as Services } from './Services';
 export { default as Button } from './Button';
 export { default as Hero } from './Hero';
+
+

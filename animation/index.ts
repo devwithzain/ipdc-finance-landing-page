f61f57fadc618200c58d2e10b7ad26b865b07530
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-01-24 09:12:58 +0500

    Exported different components

---
+++ animation/index.ts
@@ -0,0 +1,6 @@
+export { default as Magnetic } from './Magnetic';
+export { default as LinkHover } from './LinkHover';
+export { default as LogoMarquee } from './LogoMarquee';
+export { default as Sticky } from './Sticky';
+export { default as TextMask } from './TextMask';
+export { default as MobileNavbar } from './mobile/MobileNavbar';

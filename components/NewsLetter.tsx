87c6a033ac74389db03fc86755be2b7a3f26961c
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-01-24 09:17:41 +0500

    Completed

--- components/NewsLetter.tsx
+++ components/NewsLetter.tsx
@@ -1,12 +1,15 @@
+import { TextMask } from "@animation";
 import Button from "./Button";
 
 export default function NewsLetter() {
+	const phrases = ["Newsletter"];
+
 	return (
 		<section className="w-full my-[100rem] bg-[#FEF9FC]">
 			<div className="flex flex-col gap-y-[30rem] py-[100rem] flex-wrap">
 				<div className="flex flex-col gap-y-[20rem]">
-					<h1 className="text-[36rem] text-[#343434] font-bold leading-[43rem] text-center">
-						Newsletter
+					<h1 className="text-[36rem] text-[#343434] font-bold leading-[43rem] text-center tracking-wider">
+						<TextMask>{phrases}</TextMask>
 					</h1>
 					<p className="text-[#999] text-[16rem] font-normal leading-[22rem] text-center">
 						Lorem ipsum dolor sit amet, consectetur adipiscing <br /> elit. Eget
@@ -22,7 +25,10 @@ export default function NewsLetter() {
 						/>
 					</div>
 					<div>
-						<Button title="SUBSCRIBE" />
+						<Button
+							title="SUBSCRIBE"
+							className="bg-[#ED017F] text-white"
+						/>
 					</div>
 				</div>
 			</div>

1d022cff9888b17449aa152000c951f161eb6feb
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-01-24 09:43:40 +0500

    Modified

--- components/index.ts
+++ components/index.ts
@@ -6,7 +6,6 @@ export { default as Result } from './Result';
 export { default as Experience } from './Experience';
 export { default as Partner } from './Partner';
 export { default as Glance } from './Glance';
-export { default as Services } from './Services';
 export { default as Button } from './Button';
 export { default as Hero } from './Hero';
 

17126367356b819291c6c9004a7dbfbaf6f35721
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-01-24 09:13:20 +0500

    Exported different components constants

---
+++ constants/index.ts
@@ -0,0 +1,337 @@
+import { bag, c2s, carOne, circle, cube, decimal, facebook, flying, forbes, four, funny, glass, instagram, linkedin, locomotive, macys, maven, mensHealth, mrBeast, object, officestudio, one, pig, powell, rocket, scinery, silencio, three, two, user, youtube } from "@public";
+
+export const navLinks = [
+    {
+        id: 1,
+        title: "About Us",
+        href: "aboutUs"
+    },
+    {
+        id: 2,
+        title: "Campgains",
+        href: "campgains"
+    },
+];
+
+
+export const footerLinks = [
+    {
+        id: 1,
+        title: "Products",
+        links: [
+            {
+                id: 1,
+                link: '/',
+                title: "CSR Activities"
+            },
+            {
+                id: 2,
+                link: '/',
+                title: "Green Banking"
+            },
+            {
+                id: 3,
+                link: '/',
+                title: "News"
+            },
+            {
+                id: 4,
+                link: '/',
+                title: "Ongoing Campgain"
+            },
+            {
+                id: 5,
+                link: '/',
+                title: "Updates"
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: "Get Started",
+        links: [
+            {
+                id: 1,
+                link: '/',
+                title: "Career"
+            },
+            {
+                id: 2,
+                link: '/',
+                title: "Contact Us"
+            },
+            {
+                id: 3,
+                link: '/',
+                title: "Government Securities"
+            },
+            {
+                id: 4,
+                link: '/',
+                title: "Examples"
+            },
+            {
+                id: 5,
+                link: '/',
+                title: "NIS"
+            },
+        ]
+    },
+    {
+        id: 3,
+        title: "About",
+        links: [
+            {
+                id: 1,
+                link: '/',
+                title: "IPDC at a Glance"
+            },
+            {
+                id: 2,
+                link: '/',
+                title: "Mission, VIsion & Values"
+            },
+            {
+                id: 3,
+                link: '/',
+                title: "Corporate Governanace"
+            },
+            {
+                id: 4,
+                link: '/',
+                title: "Shareholders"
+            },
+            {
+                id: 5,
+                link: '/',
+                title: "Investor Relations"
+            },
+        ]
+    },
+    {
+        id: 4,
+        title: "16519",
+    },
+];
+
+export const footerIcons = [
+    {
+        id: 1,
+        img: facebook,
+        link: "www.facebook.com"
+    },
+    {
+        id: 2,
+        img: youtube,
+        link: "www.youtube.com"
+    },
+    {
+        id: 3,
+        img: linkedin,
+        link: "www.linkedin.com"
+    },
+    {
+        id: 4,
+        img: instagram,
+        link: "www.instagram.com"
+    },
+];
+
+export const resultList = [
+    {
+        id: 1,
+        title: '57.6 bn',
+        para: 'LOAN PORTFOLIO'
+    },
+    {
+        id: 2,
+        title: '0.95%',
+        para: 'CLASSIFIED LOAN PORTFOLIO'
+    },
+    {
+        id: 3,
+        title: '388.5%',
+        para: 'CLASSIFIED LOAN Coverage'
+    },
+    {
+        id: 4,
+        title: '50.4 bn',
+        para: 'Deposit'
+    },
+    {
+        id: 5,
+        title: '6.1 bn',
+        para: 'Shareholders equity'
+    },
+    {
+        id: 6,
+        title: '18.51%',
+        para: 'Capital Adequacy Ratio '
+    },
+    {
+        id: 7,
+        title: '8.5 bn',
+        para: 'Market Capitalization'
+    },
+    {
+        id: 8,
+        title: 'AAA',
+        para: 'Credit Rating'
+    },
+];
+
+export const experienceList = [
+    {
+        id: 1,
+        img: circle,
+        title: "Integrity",
+        para: "Displaying the highest level of Integrity in the way we conduct our business",
+    },
+    {
+        id: 2,
+        img: scinery,
+        title: "Demonstrate",
+        para: "Demonstrating a strong Will to Win in the market place",
+    },
+    {
+        id: 3,
+        img: object,
+        title: "Diversity",
+        para: "Promoting Diversity in the work place and community",
+    },
+    {
+        id: 4,
+        img: user,
+        title: "Teamwork",
+        para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. A semper aenean id pen",
+    },
+    {
+        id: 5,
+        img: circle,
+        title: "Collaboration",
+        para: "Displaying the highest level of Integrity in the way we conduct our business",
+    },
+    {
+        id: 6,
+        img: rocket,
+        title: "Technology",
+        para: "Harnessing the power of Technology to deliver better customer experience",
+    },
+    {
+        id: 7,
+        img: bag,
+        title: "Corporate",
+        para: "Setting the standard for the best Corporate Citizenship in the communities we work",
+    },
+    {
+        id: 8,
+        img: cube,
+        title: "Digital",
+        para: "Setting the standard for the best Corporate Citizenship in the communities we work",
+    },
+];
+
+
+export const partnerList = [
+    {
+        id: 1,
+        img: one,
+    },
+    {
+        id: 2,
+        img: two,
+    },
+    {
+        id: 3,
+        img: three,
+    },
+    {
+        id: 4,
+        img: four,
+    },
+    {
+        id: 5,
+        img: forbes
+    },
+    {
+        id: 6,
+        img: macys
+    },
+    {
+        id: 7,
+        img: mensHealth
+    },
+    {
+        id: 8,
+        img: mrBeast
+    },
+];
+
+export const servicesIcons = [
+    {
+        id: 1,
+        img: glass,
+        color: "#e3e5e7"
+    },
+    {
+        id: 2,
+        img: flying,
+        color: "#d6d7dc"
+    },
+    {
+        id: 3,
+        img: pig,
+        color: "#e3e3e3"
+    },
+    {
+        id: 4,
+        img: carOne,
+        color: "#21242b"
+    },
+];
+
+export const servicesIcons2 = [
+    {
+        id: 1,
+        img: maven,
+        color: "#d4e3ec"
+    },
+    {
+        id: 2,
+        img: powell,
+        color: "#e5e0e1"
+    },
+    {
+        id: 3,
+        img: funny,
+        color: "#d7d4cf"
+    },
+    {
+        id: 4,
+        img: decimal,
+        color: "#e1dad6"
+    },
+];
+
+export const project = [
+    {
+        title: "C2 Montreal",
+        img: c2s,
+        color: "#000000",
+    },
+    {
+        title: "Office Studio",
+        img: officestudio,
+        color: "#8C8C8C",
+    },
+    {
+        title: "Locomotive",
+        img: locomotive,
+        color: "#EFE8D3",
+    },
+    {
+        title: "Silencio",
+        img: silencio,
+        color: "#706D63",
+    },
+];

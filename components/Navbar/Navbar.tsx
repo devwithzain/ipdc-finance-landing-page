1e32b35aaf400eb611bcdd22706c4a8d62cbeef1
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-01-24 09:17:28 +0500

    Completed

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 "use client";
-import Link from "next/link";
-import Image from "next/image";
-import { useState } from "react";
+import { LinkHover, MobileNavbar } from "@animation";
 import { Button } from "@components";
-import { navVariants } from "@motion";
 import { navLinks } from "@constants";
-import { LinkHover } from "@animation";
+import { navVariants } from "@motion";
 import { logo, magnifier } from "@public";
 import { useMotionValueEvent, useScroll, motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
 	const [hidden, setHidden] = useState(false);
@@ -24,7 +24,7 @@ export default function Navbar() {
 	return (
 		<>
 			<motion.nav
-				className="w-full flex items-center justify-between h-[10vh] px-[100rem] sticky top-0 z-10 bg-white"
+				className="w-full flex items-center justify-between h-[10vh] lg:px-[100rem] px-[70rem] sticky top-0 z-10 bg-white"
 				variants={navVariants}
 				animate={hidden ? "hidden" : "vissible"}>
 				<div>
@@ -35,19 +35,19 @@ export default function Navbar() {
 						className="w-[100rem] object-cover"
 					/>
 				</div>
-				<div className="flex gap-[10rem] font-medium tracking-[1rem] uppercase">
+				<div className="lg:flex gap-[10rem] font-medium tracking-[1rem] uppercase hidden">
 					{navLinks.map((item) => (
 						<div key={item.id}>
 							<Link
-								href={`#${item.link}`}
+								href={`#${item.href}`}
 								className="text-[18rem] font-bold text-[#676767]">
 								<LinkHover>{item.title}</LinkHover>
 							</Link>
 						</div>
 					))}
 				</div>
-				<div className="flex items-center gap-[25rem]">
-					<div className="">
+				<div className="lg:flex items-center gap-[25rem] hidden">
+					<div>
 						<Image
 							src={magnifier}
 							alt="magnifier"
@@ -57,8 +57,11 @@ export default function Navbar() {
 					</div>
 					<Button
 						title="iSERVICES"
-						className="bg-[#ED017F]"
+						className="bg-[#ED017F] text-white"
 					/>
+				</div>
+				<div className="block lg:hidden">
+					<MobileNavbar />
 				</div>
 			</motion.nav>
 		</>
